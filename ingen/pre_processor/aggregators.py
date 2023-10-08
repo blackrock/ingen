07ce1e0424d5252969fbf78f5aa7193b75fbd003
Swarna Dhakad <swarna.dhakad@gmail.com>
2023-10-08 23:32:48 +0530

    add comments

--- ingen/pre_processor/aggregators.py
+++ ingen/pre_processor/aggregators.py
@@ -2,10 +2,22 @@
 #  All Rights Reserved.
 
 def groupby(config, data):
+    """
+    groups data by configured column
+
+    :param config : columns for grouping
+    :param data : data to be aggregated
+    """
     return data.groupby(config['cols'])
 
 
 def agg(config, data):
+    """
+    aggregates passed data as per configuration
+
+    :param config : configuration for aggregator
+    :param data : data to be aggregated
+    """
     return data.agg(config['operation'], config['col'])
 
 
@@ -17,7 +29,18 @@ aggregator_map = {
 
 
 def get_aggregator(aggregator_type):
+    """
+    gets an aggregator, returns aggregator function
+
+    :param aggregator_type : name/type of the aggregator
+    """
     return aggregator_map.get(aggregator_type)
 
 def add_aggregator(aggregator_type, aggregator):
+    """
+    adds a new aggregator
+
+    :param aggregator_type : name/type of the aggregator
+    :param aggregator : aggregator function
+    """
     return aggregator_map.update( {aggregator_type: aggregator} )
