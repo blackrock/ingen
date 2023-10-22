836879108709579db78c3b12d9415344586f2677
Swarna Dhakad <swarna.dhakad@gmail.com>
2023-10-22 22:47:29 +0530

    adding more information about config

--- ingen/pre_processor/aggregators.py
+++ ingen/pre_processor/aggregators.py
@@ -6,7 +6,11 @@ def groupby(config, data):
     groups data by configured column
 
     :param config : columns for grouping
-    :param data : data to be aggregated
+        config columns on which group ing has to be performed as expressed below
+        {'cols': ['acc', 'cusip']}
+        the intent here is to group on columns acc, cusip
+    :param data : data to be grouped
+
     """
     return data.groupby(config['cols'])
 
@@ -16,6 +20,10 @@ def agg(config, data):
     aggregates passed data as per configuration
 
     :param config : configuration for aggregator
+        config consist of the operation to be
+        performed and the columns on which the operation has to be performed.
+        example of config : {'operation': 'sum', 'col': 'quantity'}
+        once the aggregation is applied (example sum(quantity) here)
     :param data : data to be aggregated
     """
     return data.agg(config['operation'], config['col'])
