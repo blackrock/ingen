673c84ba29b245a424c62f1c8265342316442407
Swarna Dhakad <swarna.dhakad@gmail.com>
2023-10-31 23:38:25 +0530

    code review comments - readers

--- ingen/reader/data_source.py
+++ ingen/reader/data_source.py
@@ -11,17 +11,16 @@ class DataSource(object):
         self.__user = user
         self.__passwd = passwd
         self.__database = database
-        self.__cursors = set()
+        self.__cursor = set()
 
     def get_connection(self):
         try:
-            self.__connection = connection \
-                .MySQLConnection(host=self.__host, user=self.__user, password=self.__passwd,
+            self.__connection = connection.MySQLConnection(host=self.__host, user=self.__user, password=self.__passwd,
                                  database=self.__database) if not self.__connection else self.__connection
             return self.__connection
         except Error as e:
             raise RuntimeError("Not able to establish connection with this database.")
 
     def get_cursor(self):
-        self.__cursors = self.__connection.cursor()
-        return self.__cursors
+        self.__cursor = self.__connection.cursor()
+        return self.__cursor
