c154daed39e565718ec2c71245b046aa8e49ba11
piyush <piyushranjan95@gmail.com>
2023-11-02 18:20:29 +0530

    refactor run config

--- ingen/utils/run_configuration.py
+++ ingen/utils/run_configuration.py
@@ -12,19 +12,19 @@ class RunConfiguration:
 
     @property
     def generator(self):
-        generator_name = self.get_config_value('generator') or 'InterfaceGenerator'
+        generator_name = self.get_config_value('generator', 'InterfaceGenerator')
         return getattr(generators, generator_name)
 
     @property
     def writer(self):
-        writer_name = self.get_config_value('writer') or 'InterfaceWriter'
+        writer_name = self.get_config_value('writer', 'InterfaceWriter')
         return getattr(writers, writer_name)
 
     @property
     def formatter(self):
-        formatter_name = self.get_config_value('formatter') or 'Formatter'
+        formatter_name = self.get_config_value('formatter', 'Formatter')
         return getattr(formatters, formatter_name)
 
-    def get_config_value(self, attr):
-        if attr in self._config:
-            return self._config[attr]
+    def get_config_value(self, attr, default=None):
+        return self._config.get(attr, default)
+
