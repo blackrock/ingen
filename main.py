97eb026c66a6b451e99d2e4bd9cdc6472f4c785f
piyush <piyushranjan95@gmail.com>
2023-11-02 18:00:58 +0530

    add help text to arguments

--- main.py
+++ main.py
@@ -14,11 +14,28 @@ logger = logging.getLogger()
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_path")
-    parser.add_argument("run_date", nargs="?", default=date.today())
-    parser.add_argument("--query_params", nargs="*", action=KeyValue)
-    parser.add_argument("--interfaces")
-    parser.add_argument("--infile", type=str, help="filepath")
+    parser.add_argument("config_path", help="path to config file")
+    parser.add_argument(
+        "run_date",
+        nargs="?",
+        default=date.today(),
+        help="run date in format MMDDYYYY",
+        type=lambda d: datetime.strptime(d, "%m%d%Y"),
+    )
+    parser.add_argument(
+        "--query_params",
+        nargs="*",
+        action=KeyValue,
+        help="key value pairs to replace in sql queries",
+    )
+    parser.add_argument(
+        "--interfaces", help="comma separated list of interfaces to generate"
+    )
+    parser.add_argument(
+        "--infile",
+        type=str,
+        help="overwrite path of a file when `use_infile` is turned on in a file source",
+    )
     return parser
 
 
@@ -29,8 +46,6 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
     if args.interfaces is not None:
         args.interfaces = args.interfaces.split(",")
-    if type(args.run_date) == str:
-        args.run_date = datetime.strptime(args.run_date, "%m%d%Y")
     if args.infile:
         logger.info(
             f"Use $infile when source use_infile is turned on, overwrite source file_path: {args.infile}"
