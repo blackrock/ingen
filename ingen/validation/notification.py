b6922ba8343f7d75ad45fe692c043092687bcfe5
PIYUSH RANJAN <piyushranjan95@gmail.com>
2023-12-14 14:59:36 +0530

    [REFACTOR] Add help text to args  (#33)
    * Add help text to arguments
    
    * use os.path.basename instead of splitting string
    
    * reformat files

--- ingen/validation/notification.py
+++ ingen/validation/notification.py
@@ -2,6 +2,7 @@
 #  All Rights Reserved.
 
 import logging
+import os.path
 
 from ingen.validation.send_email import send_email
 
@@ -9,27 +10,32 @@ log = logging.getLogger()
 
 
 def get_attributes(params, validation_action):
-    validate_action_to_adr = validation_action.get('send_email')
-    run_date = params.get('run_date')
-    file_name = params.get('infile')
+    validate_action_to_adr = validation_action.get("send_email")
+    run_date = params.get("run_date")
+    file_name = params.get("infile")
     if file_name:
-        file_name = file_name.split('/')[-1]
-    validate_action_attributes = {'run_date': run_date, 'validate_action_to_adr': validate_action_to_adr,
-                                  'file_name': file_name}
+        file_name = os.path.basename(file_name)
+    validate_action_attributes = {
+        "run_date": run_date,
+        "validate_action_to_adr": validate_action_to_adr,
+        "file_name": file_name,
+    }
     return validate_action_attributes
 
 
 def email_attributes(params, validation_action, validation_summary):
     """
-        sends an email if to_address is mentioned
-        :param params : Has all the attributes required to send the email
-        :param validation_action: list of email
-        :param validation_summary: Dictionary of source name and their validation summary
+    sends an email if to_address is mentioned
+    :param params : Has all the attributes required to send the email
+    :param validation_action: list of email
+    :param validation_summary: Dictionary of source name and their validation summary
     """
     validation_action_attributes = get_attributes(params, validation_action)
-    validation_action_to_address = validation_action_attributes.get('validate_action_to_adr')
-    run_date = str(validation_action_attributes.get('run_date'))
-    file_name = str(validation_action_attributes.get('file_name'))
+    validation_action_to_address = validation_action_attributes.get(
+        "validate_action_to_adr"
+    )
+    run_date = str(validation_action_attributes.get("run_date"))
+    file_name = str(validation_action_attributes.get("file_name"))
     subject = "Data Validation Failure (" + run_date + ")"
     validation_failure = False
     email_body = str()
@@ -40,13 +46,15 @@ def email_attributes(params, validation_action, validation_summary):
 
         if validation_result:
             validation_failure = True
-            email_body += '\nValidation Status for source: ' + str(source_name) + '\n'
+            email_body += "\nValidation Status for source: " + str(source_name) + "\n"
             if file_name:
-                email_body += 'File Name: ' + file_name + '\n\n'
+                email_body += "File Name: " + file_name + "\n\n"
             for log_messages in validation_result:
-                email_body += log_messages + '\n'
+                email_body += log_messages + "\n"
 
     if validation_failure:
         send_email(validation_action_to_address, email_body, subject)
-    if 'blocker' in str(validation_summary):
-        raise ValueError(f"Error while Validating interface file for the columns having severity as blocker")
+    if "blocker" in str(validation_summary):
+        raise ValueError(
+            f"Error while Validating interface file for the columns having severity as blocker"
+        )
